4eaf546373fbafe79dc020bc17b96d62c27873e3
Kumpukwe <kululanga1804@gmail.com>
2026-03-23 16:26:54 +0200

    Serve frontend from backend

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname)); // ← ADD THIS LINE HERE
 
 // Determine environment
 const isDevelopment = process.env.NODE_ENV === 'development';
